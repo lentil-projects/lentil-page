3d66958d4895a638c2b2b1ffd8eeddc7f006c784
crochetcode <fred.davison@outlook.com>
2020-12-28 17:47:04 -0600

    refactor component tree

--- src/components/brackets/Championship.jsx
+++ src/components/brackets/Championship.jsx
@@ -1,40 +1,6 @@
 import React from 'react';
-import { teamNames, playerNames } from './constants/teams';
-import { results } from './constants/results';
 
-const returnResult = (team, game, newGame, newIndex) => {
-    if (team === 'winner' && results[game].winner) {
-        return (
-            <div className='row'>
-                {returnScore(newGame, newIndex)}
-                <div className='seed small-seed inline'>{playerNames[results[game].winner][0].seed}</div>
-                <div className='team inline'>{teamNames[results[game].winner]}</div>
-            </div>
-        )
-    }
-    if (team === 'loser' && results[game].loser) {
-        return (
-            <div className='row'>
-                {returnScore(newGame, newIndex)}
-                <div className='seed small-seed inline'>{playerNames[results[game].loser][0].seed}</div>
-                <div className='team inline'>{teamNames[results[game].loser]}</div>
-            </div>
-        )
-    }
-    return (<div className="team"><em>{`${team} of ${game.toUpperCase()}`}</em></div>)
-}
-
-const returnScore = (game, index) => {
-    if (game === undefined || index === undefined) {
-        return
-    }
-    if (!results[game].score.length) {
-        return (<div className='inline score'>0</div>)
-    }
-    return (<div className='inline score'>{results[game].score[index]}</div>)
-}
-
-export const Championship = () => {
+export const ChampionshipBrackets = props => {
     return (
         <div className='bracket-group championship'>
             <div className="bracket-panel little-bracket">
@@ -45,8 +11,8 @@ export const Championship = () => {
                         <div className="game">
                             <div className='seed inline gray'>M</div>
                             <div className="inline pair">
-                                {returnResult('loser', 'i', 'm', 0)}
-                                {returnResult('winner', 'l', 'm', 1)}
+                                {props.returnResult('loser', 'i', 'm', 0)}
+                                {props.returnResult('winner', 'l', 'm', 1)}
                             </div>
                         </div>
                     </div>
@@ -58,7 +24,7 @@ export const Championship = () => {
                         <div className="space"></div>
                         <div className="game">
                             <div className="pair">
-                                {returnResult('winner', 'm')}
+                                {props.returnResult('winner', 'm')}
                             </div>
                         </div>
                     </div>
@@ -72,8 +38,8 @@ export const Championship = () => {
                         <div className="game">
                             <div className='seed inline gray'>N</div>
                             <div className="inline pair">
-                                {returnResult('winner', 'i', 'n', 0)}
-                                {returnResult('winner', 'm', 'n', 1)}
+                                {props.returnResult('winner', 'i', 'n', 0)}
+                                {props.returnResult('winner', 'm', 'n', 1)}
                             </div>
                         </div>
                     </div>
@@ -85,7 +51,7 @@ export const Championship = () => {
                         <div className="space"></div>
                         <div className="game">
                             <div className="pair">
-                                {returnResult('winner', 'n')}
+                                {props.returnResult('winner', 'n')}
                             </div>
                         </div>
                     </div>
@@ -99,8 +65,8 @@ export const Championship = () => {
                         <div className="game">
                             <div className='seed inline gray'>O</div>
                             <div className="inline pair">
-                                {returnResult('winner', 'n', 'o', 0)}
-                                {returnResult('loser', 'n', 'o', 1)}
+                                {props.returnResult('winner', 'n', 'o', 0)}
+                                {props.returnResult('loser', 'n', 'o', 1)}
                             </div>
                         </div>
                     </div>
@@ -112,7 +78,7 @@ export const Championship = () => {
                         <div className="space"></div>
                         <div className="game">
                             <div className="pair">
-                                {returnResult('winner', 'o')}
+                                {props.returnResult('winner', 'o')}
                             </div>
                         </div>
                     </div>
