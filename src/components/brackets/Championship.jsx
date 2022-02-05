85a2a424fc5923dade5cfe6224bc43fe1e169f3d
crochetcode <fred.davison@outlook.com>
2022-02-05 12:55:09 -0600

    clean up

--- src/components/brackets/Championship.jsx
+++ src/components/brackets/Championship.jsx
@@ -7,7 +7,7 @@ export const ChampionshipBrackets = props => {
                 <h2>The Stir-Frynals</h2>
                 <div className="brackets">
                     <div className="round">
-                        <div className="heading3">Feb 14*</div>
+                        <div className="heading3">Feb 14</div>
                         <p>best of 5</p>
                         <div className='game-group'>
                             <div className='seed inline gray'>M</div>
@@ -37,7 +37,7 @@ export const ChampionshipBrackets = props => {
                 <h2>The Lentil SOUPer Bowl</h2>
                 <div className="brackets">
                     <div className="round">
-                        <div className="heading3">Feb 21*</div>
+                        <div className="heading3">Feb 21</div>
                         <p>best of 7</p>
                         <div className='game-group'>
                             <div className='seed inline gray'>N</div>
@@ -62,40 +62,6 @@ export const ChampionshipBrackets = props => {
                         </div>
                     </div>
                 </div>
-            </div>
-            {/* <div className="bracket-panel little-bracket">
-                <h2>The Lentil SOUPer Bowl 2** </h2>
-                <div className="brackets">
-                    <div className="round">
-                        <div className="heading3">Feb 28*</div>
-                        <p>best of 7</p>
-                        <div className='game-group'>
-                            <div className='seed inline gray'>O</div>
-                            <div className='game'>
-                                {props.returnResult('winner', 'n', 'o', 0)}
-                                {props.returnResult('loser', 'n', 'o', 1)}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="space-col mobile-hide">
-                        <div className="space"></div>
-                        <div className="space"></div>
-                        <div className="arrow">{'--'}</div>
-                    </div>
-                    <div className="round mobile-hide">
-                        <div className="space"></div>
-                        <div className="space"></div>
-                        <div className="game">
-                            <div className="pair">
-                                {props.returnResult('winner', 'o')}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-            <div className='mobile'>
-            <p className='white note'>* dates reflect when games are cast</p>
-            {/* <p className='white note'>**SOUPer Bowl rematch Feb. 28th if needed</p> */}
             </div>
         </div>
     )
