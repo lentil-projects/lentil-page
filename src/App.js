ce096ec898dd5fb6b8f7668fcab72ecaf9cb14fa
crochetcode <fred.davison@outlook.com>
2022-02-05 12:47:09 -0600

    add lentil home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
 import './Civs.css';
-import { Switch, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import { Brackets } from './pages/Brackets';
-import { Teams } from './pages/Teams';
-import {Faceoff} from './pages/Faceoff';
-import {About} from './pages/About';
-
+import { Routes, Route } from 'react-router-dom';
+import { Lentil } from './lentil';
+import { LentilCupRoutes } from './routes/lentil-cup';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Brackets} />
-        <Route path='/teams' component={Teams} />
-        <Route path='/faceoff' component={Faceoff} />
-        <Route path='/about' component={About} />
-      </Switch>
-      <a href='https://github.com/lentil-projects/lentil-cup' target='_blank' rel='noreferrer'>
-        <i className="fab fa-github"></i>
-      </a>
+      <Routes>
+        <Route path='*' element={<Lentil/>} />
+        <Route path='/lentil-cup/*' element={<LentilCupRoutes/>} />
+      </Routes>
     </div>
   );
 }
