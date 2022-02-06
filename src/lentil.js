78acf57ce24c04e618f91ec81d723bc3ddf9f6e7
crochetcode <fred.davison@outlook.com>
2022-02-06 16:44:09 -0600

    lint

--- src/lentil.js
+++ src/lentil.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom"
 export const Lentil = () => {
     return (
         <div className='lentil'>
-        <div className='lentil-div'>
-            <h1>Lentil_AoE</h1>
-            <a href='https://www.twitch.tv/lentil_aoe' target='_blank' rel="noreferrer">
-            <button><i className="fab fa-twitch large-logo"></i> Twitch</button>
+            <div className='lentil-div'>
+                <h1>Lentil_AoE</h1>
+                <a href='https://www.twitch.tv/lentil_aoe' target='_blank' rel="noreferrer">
+                    <button><i className="fab fa-twitch large-logo"></i> Twitch</button>
                 </a>
-            <Link to='/lentil-cup'>
-                <button>Lentil Cup One</button>
-            </Link>
-        </div>
+                <Link to='/lentil-cup'>
+                    <button>Lentil Cup One</button>
+                </Link>
+            </div>
         </div>
     )
 }
