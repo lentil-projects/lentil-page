b584c69e81939b80084a2a26f16605f034ed21ca
crochetcode <fred.davison@outlook.com>
2022-02-15 18:06:51 -0600

    add players

--- src/pages/tournaments.js
+++ src/pages/tournaments.js
@@ -7,8 +7,8 @@ export const Tournaments = () => {
                 <h1>Lentil Cup 1.5</h1>
                 <h2>About</h2>
                 <ul>
-                    <li>Single elimination?</li>
-                    <li>Best of 5?</li>
+                    <li>Double elimination</li>
+                    <li>First Round Best of 3</li>
                 </ul>
                 <br />
                 <p>Each round a player can choose one of the following game modes:</p>
@@ -17,9 +17,10 @@ export const Tournaments = () => {
                     <li>Game Speed? (Turbo, etc)</li>
                 </ul>
                 <h2>Maps</h2>
+                <p>2 map bans, 2 home maps</p>
                 <p>Game 1: MegaRandom</p>
                 <br />
-                <h3>Map Pool (1 ban?):</h3>
+                <h3>Map Pool:</h3>
                 <div className='flex'>
                     <ul>
                         <li>Bog Socotra</li>
@@ -37,32 +38,18 @@ export const Tournaments = () => {
                 <br />
                 <p>Maps will be available <a href='https://github.com/lentil-projects/lc1.5-maps' target='_blank' rel='noreferrer' className='purple'>here</a>.</p>
                 <h2>Players</h2>
-                {/* <div className='flex'>
+                <div className='flex'>
                     <ul>
                         <li>Albatross Rex</li>
-                        <li>FireEMT?</li>
+                        <li>FireEMT</li>
                         <li>Frizune</li>
                         <li>Lord's Servant</li>
                     </ul>
                     <ul>
-                        <li>JtotheRebellion?</li>
                         <li>Master Beef</li>
                         <li>Monkeyninja</li>
                         <li>Nathan Skip</li>
-                    </ul>
-                </div> */}
-                <div className='flex'>
-                    <ul>
-                        <li>Player 1</li>
-                        <li>Player 2</li>
-                        <li>Player 3</li>
-                        <li>Player 4</li>
-                    </ul>
-                    <ul>
-                        <li>Player 5</li>
-                        <li>Player 6</li>
-                        <li>Player 7</li>
-                        <li>Player 8</li>
+                        <li>RedBearded</li>
                     </ul>
                 </div>
                 <br />
