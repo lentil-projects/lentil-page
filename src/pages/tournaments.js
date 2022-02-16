44b3a00e9df49735824411c2bcb0580f4d20df3a
crochetcode <fred.davison@outlook.com>
2022-02-15 18:57:38 -0600

    update about

--- src/pages/tournaments.js
+++ src/pages/tournaments.js
@@ -10,14 +10,14 @@ export const Tournaments = () => {
                     <li>Double elimination</li>
                     <li>First Round Best of 3</li>
                 </ul>
-                <br />
+                {/* <br />
                 <p>Each round a player can choose one of the following game modes:</p>
                 <ul>
                     <li>King of the Hill</li>
                     <li>Game Speed? (Turbo, etc)</li>
-                </ul>
+                </ul> */}
                 <h2>Maps</h2>
-                <p>2 map bans, 2 home maps</p>
+                <p>2 map bans each, 2 home maps each</p>
                 <p>Game 1: MegaRandom</p>
                 <br />
                 <h3>Map Pool:</h3>
@@ -27,16 +27,21 @@ export const Tournaments = () => {
                         <li>Canberra</li>
                         <li>Cliff Hell</li>
                         <li>Crochetbia</li>
+                        <li>Golden Swamp</li>
                     </ul>
                     <ul>
-                        <li>Golden Swamp</li>
+                        <li>Michi</li>
+                        <li>Ponds</li>
                         <li>Reverse Arena</li>
                         <li>WWC Nomad</li>
-                        <li>ZeWall?</li>
+                        <li>ZeWall</li>
                     </ul>
                 </div>
                 <br />
                 <p>Maps will be available <a href='https://github.com/lentil-projects/lc1.5-maps' target='_blank' rel='noreferrer' className='purple'>here</a>.</p>
+                <h2>Civs</h2>
+                <p>2 global bans each, 4 civ picks each</p>
+                <br />
                 <h2>Players</h2>
                 <div className='flex'>
                     <ul>
