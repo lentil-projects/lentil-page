bf8f5e37790e58be68c9fe5d7fb42bf4a446729c
crochetcode <fred.davison@outlook.com>
2022-03-04 14:44:27 -0600

    parallax lol

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './Civs.css';
 import './style/header.css';
 import './style/maps.css';
+import './style/landing.css';
 import { Routes, Route } from 'react-router-dom';
 import { Lentil } from './pages/lentil';
 import { TournamentRoutes } from './routes/tournaments';
--- src/pages/lentil.js
+++ src/pages/lentil.js
@@ -1,9 +1,63 @@
 import { Link } from "react-router-dom"
+import untitled1 from '../assets/landing/untitled1.png';
+import untitled2 from '../assets/landing/untitled2.png';
+import untitled3 from '../assets/landing/untitled3.png';
 
 export const Lentil = () => {
     return (
-        <div className='lentil'>
-            <div className='lentil-div'>
+        <>
+
+
+            <div className="landing">
+                <div className="parallax">
+                    <div className=" parallax__layer parallax__layer--back">
+                        <div className="diptych dtwo">
+                            <img src={untitled2} alt="" />
+                        </div>
+                        <div className="float">
+                            <img src={untitled3} alt="" />
+                            <img src={untitled3} alt="" />
+                            <img src={untitled3} alt="" />
+                        </div>
+                    </div>
+                    <div class="parallax__layer parallax__layer--base">
+                    <div className="diptych">
+                        <a href='https://www.twitch.tv/lentil_aoe' target='_blank' rel="noreferrer">
+                            <p>{'Lentil_AoE '}<i className="fab fa-twitch"></i></p>
+                        </a>
+                        <p className='right'>Lentil Cup</p>
+                    </div>
+                    <hr />
+                    <div className="diptych">
+                        <p>@lentildestroyer</p>
+                        <p className='right'>1</p>
+                    </div>
+                    <hr />
+                    <div className="diptych">
+                        <p>@lentilgarf</p>
+                        <p className='right'>1.5</p>
+                    </div>
+                    <hr />
+                        <div className="float">
+                            <img src={untitled3} alt="" />
+                            <img src={untitled3} alt="" />
+                            <img src={untitled3} alt="" />
+                        </div>
+                        <div className="diptych done">
+                            <img src={untitled1} alt="" />
+                        </div>
+                        <div className="diptych dthree">
+                            <img src={untitled2} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+            {/* <div className='lentil-div'>
                 <h1>Lentil_AoE</h1>
                 <a href='https://www.twitch.tv/lentil_aoe' target='_blank' rel="noreferrer">
                     <button><i className="fab fa-twitch"></i>Lentil Twitch</button>
@@ -17,7 +71,7 @@ export const Lentil = () => {
                 <Link to='/tournaments'>
                     <button>Lentil Tournaments</button>
                 </Link>
-            </div>
-        </div>
+            </div> */}
+        </>
     )
 }
