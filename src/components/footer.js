f8824eb5485214508f0a99f41b70a2855da5fc59
crochetcode <fred.davison@outlook.com>
2022-06-13 09:32:28 -0500

    update crochet link, format header, make footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <nav>
+                <p>website by
+                    <a href='https://crochetaoe.dev' target='_blank' rel="noreferrer">{' crochet'}</a>
+                </p>
+            </nav>
+        </footer>
+    )
+}
